c5306fd554c089845088029358dadba62bb6d6de
JasmineD93 <Jasmine031611@gmail.com>
2017-04-26 16:00:43 -0500

    Add files via upload

---
+++ nested.js
@@ -0,0 +1,12 @@
+var post = [
+	{
+		title: "Cats are amazing",
+		author: "Jasmine",
+		comments: ["Awesome!"]
+	},
+	{
+		title: "Robots are awesome",
+		author: "Domo",
+		comments: ["I agree","love it!"]
+	}
+]
